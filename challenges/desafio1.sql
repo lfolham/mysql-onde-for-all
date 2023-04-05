f575f83ac9f52ea73de6bccc86367294655c9146
lfolham <lucasfolha@icloud.com>
2023-04-05 15:08:35 -0300

    task 1

--- challenges/desafio1.sql
+++ challenges/desafio1.sql
@@ -7,77 +7,69 @@ CREATE TABLE Plan (
   plan VARCHAR(50) NOT NULL,
   value_plan INT NOT NULL,
   PRIMARY KEY (id_Plan))
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 CREATE TABLE User (
-  `id_User` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(50) NOT NULL,
-  `age` INT NOT NULL,
+  id_User INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  age INT NOT NULL,
 	id_Plan INT NOT NULL,
-   subscribe INT NOT NULL,
-  PRIMARY KEY (`id_User`),
-  INDEX `id_Plan_idx` (`id_Plan` ASC) VISIBLE,
-    FOREIGN KEY (`id_Plan`)
-    REFERENCES `Plan` (`id_Plan`)
+  subscribe INT NOT NULL,
+  PRIMARY KEY (id_User),
+  INDEX id_Plan_idx (id_Plan ASC) VISIBLE,
+    FOREIGN KEY (id_Plan)
+    REFERENCES Plan (id_Plan)
     )
-ENGINE = InnoDB;
+  ENGINE = InnoDB;
 
 CREATE TABLE Artist (
-  `id_Artist` INT NOT NULL AUTO_INCREMENT,
-  `artist_name` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`id_Artist`))
-ENGINE = InnoDB;
-
-CREATE TABLE Albums (
-  `id_Albums` INT NOT NULL AUTO_INCREMENT,
-  `album` VARCHAR(50) NOT NULL,
-  `id_Artist` INT NOT NULL,
-  `release_year` INT NOT NULL,
-  PRIMARY KEY (`id_Albums`),
-  CONSTRAINT `id_Artist`
-    FOREIGN KEY (`id_Artist`)
-    REFERENCES Artist (`id_Artist`)
-    )
-ENGINE = InnoDB;
-
-CREATE TABLE Songs (
-  `id_Songs` INT NOT NULL AUTO_INCREMENT,
-  `songs` VARCHAR(50) NOT NULL,
-  `duration` INT NOT NULL,
-  `id_Albums` INT NOT NULL,
-  FOREIGN KEY (id_Albums)
-  REFERENCES Albums (id_Albums),
-  PRIMARY KEY (`id_Songs`))
-ENGINE = InnoDB;
+  id_Artist INT NOT NULL AUTO_INCREMENT,
+  artist_name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id_Artist))
+  ENGINE = InnoDB;
 
 CREATE TABLE Following (
   id_User INT NOT NULL,
   id_Artist INT NOT NULL,
-  PRIMARY KEY (id_User, id_Artist),
+  CONSTRAINT PRIMARY KEY (id_User, id_Artist),
     FOREIGN KEY (id_Artist)
-    REFERENCES Artist (id_Artist)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+      REFERENCES Artist (id_Artist),
     FOREIGN KEY (id_User)
     REFERENCES User (id_User)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ) ENGINE = InnoDB;
+
+CREATE TABLE Albums (
+  id_Albums INT NOT NULL AUTO_INCREMENT,
+  album VARCHAR(50) NOT NULL,
+  id_Artist INT NOT NULL,
+  release_year INT NOT NULL,
+  PRIMARY KEY (id_Albums),
+  CONSTRAINT id_Artist
+    FOREIGN KEY (id_Artist)
+      REFERENCES Artist (id_Artist)
+    )
+ENGINE = InnoDB;
+
+CREATE TABLE Songs (
+  id_Songs INT NOT NULL AUTO_INCREMENT,
+  songs VARCHAR(50) NOT NULL,
+  id_Albums INT NOT NULL,
+  duration INT NOT NULL,
+  FOREIGN KEY (id_Albums)
+    REFERENCES Albums (id_Albums),
+  PRIMARY KEY (id_Songs))
 ENGINE = InnoDB;
 
 CREATE TABLE History (
   id_User INT NOT NULL,
   id_Songs INT NOT NULL,
   date_Reproduction DATETIME NOT NULL,
-  PRIMARY KEY (id_User, id_Songs),
+  CONSTRAINT PRIMARY KEY (id_User, id_Songs),
     FOREIGN KEY (id_User)
-    REFERENCES User (id_User)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+      REFERENCES User (id_User),
     FOREIGN KEY (id_Songs)
-    REFERENCES Songs (id_Songs)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+      REFERENCES Songs (id_Songs)
+)ENGINE = InnoDB;
 
 
 INSERT INTO Plan (plan, value_plan)
@@ -135,20 +127,20 @@ INSERT INTO User (name, age, id_Plan, subscribe)
 
   INSERT INTO Following (id_User, id_Artist)
     VALUES
-      (1, 1),
-      (1, 2),
-      (1, 3),
-      (2, 1),
-      (2, 3),
-      (3, 2),
-      (4, 4),
-      (5, 5),
-      (5, 6),
-      (6, 6),
-      (6, 1),
-      (7, 6),
-      (9, 3),
-      (10, 2);
+      ('1', '1'),
+      ('1', '2'),
+      ('1', '3'),
+      ('2', '1'),
+      ('2', '3'),
+      ('3', '2'),
+      ('4', '4'),
+      ('5', '5'),
+      ('5', '6'),
+      ('6', '6'),
+      ('6', '1'),
+      ('7', '6'),
+      ('9', '3'),
+      ('10', '2');
 
   INSERT INTO History (id_User, id_Songs, date_Reproduction)
     VALUES
@@ -168,6 +160,3 @@ INSERT INTO User (name, age, id_Plan, subscribe)
       (8, 4, '2012-03-17 14:56:41'),
       (9, 9, '2022-02-24 21:14:22'),
       (10, 3, '2015-12-13 08:30:22');
-      
-      
-
